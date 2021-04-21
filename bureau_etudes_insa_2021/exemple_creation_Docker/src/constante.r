504bcd866a16c11f7c8e9816cb9ed09845cd22f2
minhtunguyen <minh-tu.nguyen@hupi.fr>
2021-04-21 09:39:59 +0200

    ajout ecriture csv en local

--- bureau_etudes_insa_2021/exemple_creation_Docker/src/constante.r
+++ bureau_etudes_insa_2021/exemple_creation_Docker/src/constante.r
@@ -3,8 +3,11 @@
 mongo_host = Sys.getenv("MONGO_HOST")
 customer = Sys.getenv("MONGO_USER")
 password = Sys.getenv("MONGO_PASSWORD")
-collection_sortie = "test"
+collection_name = "collection_sortie.csv"
 
 ## Constantes HDFS : 
-path_hdfs = Sys.getenv("PATH_HDFS")
-
+hdfs_url = Sys.getenv("HDFS_URL")
+hdfs_port = Sys.getenv("HDFS_PORT")
+hdfs_username = Sys.getenv("HDFS_USERNAME")
+input_directory = Sys.getenv("INPUT_DIRECTORY")
+output_directory = Sys.getenv("OUTPUT_DIRECTORY")
